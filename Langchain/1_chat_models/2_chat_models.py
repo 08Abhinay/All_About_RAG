63b5796e1c4203d3fe6984f597947037f40e337a
Abhinay Shankar Belde <142843743+08Abhinay@users.noreply.github.com>
2025-09-28 23:13:46 -0400

    Understanding basics of chat-models

--- Langchain/1_chat_models/2_chat_models.py
+++ Langchain/1_chat_models/2_chat_models.py
@@ -10,6 +10,8 @@ llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 messages = [
     SystemMessage(content="You are a helpful assistant that translates English to French."),
     HumanMessage(content="Translate the following sentence: 'I love programming.'"),
+    AIMessage(content="J'adore la programmation."),
+    HumanMessage(content="Translate the following sentence: 'The weather is nice today.'"),
 ]
 
 response = llm.invoke(messages)
---
+++ Langchain/1_chat_models/3_chat_models-alternate-models.py
@@ -0,0 +1,2 @@
+from langchain_gemini import ChatGemini
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage  
