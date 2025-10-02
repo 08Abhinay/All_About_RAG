8706b552582642810c92e15834a18e4fb494a543
Abhinay Shankar Belde <142843743+08Abhinay@users.noreply.github.com>
2025-10-02 18:41:41 -0400

    Understood Prompt templates

---
+++ Langchain/2_prompt_templates/1_prompt_templates.py
@@ -0,0 +1,37 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+import os   
+
+
+load_dotenv()  # Load environment variables from .env file
+
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+
+template = "Write an {tone} email to {company} about {position} position, mentioning your {skills}."
+
+prompt_template = ChatPromptTemplate.from_template(template)
+
+prompt = prompt_template.invoke({
+    "tone": "professional",
+    "company": "Google",
+    "position": "Software Engineer",
+    "skills": "Python, Machine Learning, and Problem-Solving"
+})
+
+result = llm.invoke(prompt)
+print(result.content)   
+
+print("---------------------------------------------------")
+
+messages = [
+    ("system", "You are a helpful assistant who helps be make friends easily through {small_talk}."),
+    ("human", "Hi! I'm {name}. Can you help me make friends?"),
+]
+chat_prompt = ChatPromptTemplate.from_messages(messages)
+prompt_2 = chat_prompt.invoke({
+    "small_talk": "small talk",
+    "name": "Alice"
+})
+result = llm.invoke(prompt_2)
+print(result.content)   
